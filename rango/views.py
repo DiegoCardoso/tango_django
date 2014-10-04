6218defa412688ecbd194cc1d99e6c6d7f54155e
Diego Cardoso <diegopc86@gmail.com>
2014-10-05 01:22:41 +0200

    Tutorial up to chapter #7

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from rango.models import Category, Page
 from rango.help import encode_category_name, decode_category_name
+from rango.forms import CategoryForm, PageForm
 
 def index(request):
 	#Request the context of the request
@@ -44,9 +45,62 @@ def category(request, category_name_url):
 
 		pages = Page.objects.filter(category=category)
 
+		context_dict['category_name_url'] = category_name_url
 		context_dict['pages'] = pages
 		context_dict['category'] = category
 	except Category.DoesNotExist:
 		pass
 	
 	return render_to_response('rango/category.html', context_dict, context)
+
+def add_category(request):
+	context = RequestContext(request)
+
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit=True)
+
+			return redirect('index')
+		else:
+			pass
+			# print form.errors
+
+	else:
+		form = CategoryForm()
+
+	return render_to_response('rango/add_category.html', {'form': form}, context)
+
+def add_page(request, category_name_url):
+	context = RequestContext(request)	
+
+	category_name = decode_category_name(category_name_url)
+
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		
+		if form.is_valid():
+			page = form.save(commit=False)
+
+			try:
+				cat = Category.objects.get(name=category_name)
+				page.category = cat
+			except Category.DoesNotExist:
+				return render_to_response('rando/add_category', {}, context)
+
+			page.views = 0
+
+			page.save()
+
+			return redirect('category', category_name_url= category_name_url)
+		else:
+			pass
+	else:
+		form = PageForm()
+
+	return render_to_response('rango/add_page.html', {'category_name_url': category_name_url, 'category_name': category_name, 'form': form}, context)
+
+
+
+
